a71e3b30a7c34d6b382a653916fcbf718eeec9a6
liam <liam@coredot.io>
2023-07-31 19:15:51 +0900

    footer수정

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 
-import telegramLogo from "../../assets/icons/icon-tele-nor.png";
 import twitterLogo from "../../assets/icons/icon-twitter-nor.png";
-import instagramLogo from "../../assets/icons/icon-insta-nor.png";
-import discordLogo from "../../assets/icons/icon-discord-nor.png";
+import emailLogo from "../../assets/icons/icon-email-nor.png";
 
 import styles from "./Footer.module.scss";
 
@@ -11,42 +9,14 @@ const Footer: React.FC = () => {
 
     return (
         <div className={styles.footerContainer}>
-            <div className={styles.linkContainer}>
-                <div className={styles.termContainer}>
-                    <a className={styles.item}>
-                        서비스이용약관
-                    </a>
-                    <a className={styles.item}>
-                        support@dsclabel.co.kr
-                    </a>
-                </div>
+            <div className={styles.linkContainer}> 
                 <div className={styles.snsContainer}>
-                    <a className={styles.item} href="https://t.me/dogesoundclub" target="_blank" rel="noreferrer">
-                        <img src={telegramLogo} alt="telegram" />
+                    <a className={styles.item} href="mailto:lghnh_nft@lghnh.com" target="_blank" rel="noreferrer">
+                        <img src={emailLogo} alt="email" />
                     </a>
-                    <a className={styles.item} href="https://twitter.com/dogesoundclub" target="_blank" rel="noreferrer">
+                    <a className={styles.item} href="https://twitter.com/LGHNH_NFT" target="_blank" rel="noreferrer">
                         <img src={twitterLogo} alt="twitter" />
                     </a>
-                    <a className={styles.item} href="https://www.instagram.com/biasedmatecycleshop/" target="_blank" rel="noreferrer">
-                        <img src={instagramLogo} alt="instagram" />
-                    </a>
-                    <a className={styles.item} href="https://discord.com/invite/dogesoundclub" target="_blank" rel="noreferrer">
-                        <img src={discordLogo} alt="discord" />
-                    </a>
-                </div>
-            </div>
-            <div className={styles.incContainer}>
-                DSC LABEL Inc.
-            </div>
-            <div className={styles.bottomContainer}>
-                <div className={styles.textContainer}>
-                    <div>
-                        주식회사 디에스씨레이블 | 대표이사 : 권태홍 | 사업자번호 : 838-86-02498 | 개인정보보호책임자 : 권태홍<br />
-                        주소 : 서울시 성동구 아차산로5길 26, 802호 | support@dsclabel.co.kr
-                    </div>
-                    <div className={styles.copyright}>
-                        Copyright @2022 DSC LABEL Inc. ALL RIGHTS RESERVED.
-                    </div>
                 </div>
             </div>
         </div>
